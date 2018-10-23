c4379b5e81b84b7d2f810d869295f944103f4f49
squidlywizard <ztdenna@hotmail.com>
2018-10-23 08:29:17 -0600

    connected view to control

--- ZachDAlgorithm/control/AlgorithmViewController.swift
+++ ZachDAlgorithm/control/AlgorithmViewController.swift
@@ -20,6 +20,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBOutlet weak var image: UIImageView!
+    
+    @IBOutlet weak var Text: UILabel!
 }
 
