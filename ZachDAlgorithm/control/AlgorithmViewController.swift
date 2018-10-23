eb62e46ff8f0eb438bbb0217f9a5876633d814f5
squidlywizard <ztdenna@hotmail.com>
2018-10-23 09:10:38 -0600

    I honestly Do not know

--- ZachDAlgorithm/control/AlgorithmViewController.swift
+++ ZachDAlgorithm/control/AlgorithmViewController.swift
@@ -19,7 +19,6 @@ public class ViewController: UIViewController
         //calling the format algorithm
     }
 
-    override func didReceiveMemoryWarning() {
      public override func didReceiveMemoryWarning() -> Void
     {
         super.didReceiveMemoryWarning()
@@ -27,25 +26,41 @@ public class ViewController: UIViewController
     }
     private func formatAlgorithm() -> Void
     {
-        let one = "1Beggin by opening your xcode program, hitting create a new Xcode project, next"
+        let title : String = "Xcode"
+        let one : String = "1Beggin by opening your xcode program, hitting create a new Xcode project, next"
         
-        let two = "2Next give your application a name and click the blue buttons till your at a main page of sorts"
-        let three = "3Next arrange your files in the top left by command clicking and making a selection out of by left clicking, they should look like bellow <n>"
-        let four = "4Next click the blue file at the top left, click choose info.plist File click the thing that says info.plist and choose"
-        let five = "5Lastly click main.storyboard and your ready to code"
+        let two : String = "2Next give your application a name and click the blue buttons till your at a main page of sorts"
+        let three : String = "3Next arrange your files in the top left by command clicking and making a selection out of by left clicking, they should look like bellow <n>"
+        let four : String = "4Next click the blue file at the top left, click choose info.plist File click the thing that says info.plist and choose"
+        let five : String = "5Lastly click main.storyboard and your ready to code"
         let algorithm = [one, two, three, four, five]
         
-        let bullet : String = "👺"
+        let attributesDictionary = [NSAttributedStringKey.font : algorithmText.font]
+        let fullAttributedString = NSMutableAttributedString(string: title, attributes: attributesDictionary)
+        
+        for step in algorithm
+        {
+           let bullet : String = "👺"
+            let formattedStep :String = "\n\(bullet) \(step)"
+            let attributedStringStep : NSMutableAttributedString = NSMutableAttributedString(string: formattedStep)
+            let outlineStyle = createOutlineStyle()
+            
+            attributedStringStep.addAttributes(NSAttributedString.Key.paragraphStyle : outlineStyle], range: NSMakeRange(0, attributedStringStep.length))
+            
+            fullAttributedString.append(attributedStringStep)
+        }
+        
     }
     
-    privatefunc createOutlineStyle() -> NSParagraphStyle
+    private func createOutlineStyle() -> NSParagraphStyle
     {
-    let outlineStyle : NSMutableParagraphStyle = NSMutablePalagraphStyle()
+    let outlineStyle : NSMutableParagraphStyle = NSMutableParagraphStyle()
     
     outlineStyle.alignment = .left
     outlineStyle.defaultTabInterval = 15
     outlineStyle.firstLineHeadIndent = 20
     outlineStyle.headIndent = 35
+        return outlineStyle
     }
 
     @IBOutlet weak var image: UIImageView!
